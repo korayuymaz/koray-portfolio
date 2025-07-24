07c5290bbca97d3ec0846472599f88f6e10b305e
Koray <koray.uymaz@agesa.com.tr>
2025-07-24 11:20:43 +0300

    Unused variable removed

--- src/components/project-components/project-card.tsx
+++ src/components/project-components/project-card.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { getRandomColor, getRandomGradient } from "@/lib/random-color";
+import { getRandomGradient } from "@/lib/random-color";
 import Image from "next/image";
 
 type Props = {
